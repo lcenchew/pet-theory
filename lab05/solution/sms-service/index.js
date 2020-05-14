a34c2117d8a3eb4f571c64cb2f6a77166692c1dd
lcenchew <lcenchew@outlook.com>
2020-05-15 02:15:47 +0800

    Update index.js

--- lab05/solution/sms-service/index.js
+++ lab05/solution/sms-service/index.js
@@ -1,21 +1,32 @@
-const util = require('./common/util.js');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log('Listening on port', port);
 });
 
 app.post('/', async (req, res) => {
-  const labReport = util.decodeBase64Json(req.body.message.data);
+  const labReport = decodeBase64Json(req.body.message.data);
   try {
-    const status = await util.attemptFlakeyOperation();
+    console.log(`SMS Service: Report ${labReport.id} trying...`);
+    sendSms();
+
+    console.log(`SMS Service: Report ${labReport.id} success :-)`);    
     res.status(204).send();
   }
   catch (ex) {
+    console.log(`SMS Service: Report ${labReport.id} failure: ${ex}`);
     res.status(500).send();
   }
 })
 
+function decodeBase64Json(data) {
+  return JSON.parse(Buffer.from(data, 'base64').toString());
+}
+
+function sendSms() {
+  console.log('Sending SMS');
+}
